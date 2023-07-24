372cd036c888c1308de31775b3ddc321f59dd952
Andre Esteves Perrone <contato@artneo.com.br>
2023-07-24 16:30:08 -0300

    footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,1 +1,28 @@
-<h2>Footer</h2>
+<footer class="footer">
+  <div class="footer__container container">
+    <p>© Mercado Local, 2023</p>
+    <p>Desenvolvido por <a href="https://artneo.com.br">Artneo</a></p>
+  </div>
+</footer>
+
+<style>
+  .footer__container {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding-top: 24px;
+    padding-bottom: 24px;
+    flex-wrap: wrap;
+    gap: 8px;
+  }
+  p {
+    font-size: calc(14 / 16 * 1rem);
+    color: var(--principal-2);
+  }
+  p a {
+    color: var(--principal-2);
+  }
+  p a:hover {
+    text-decoration: underline;
+  }
+</style>
