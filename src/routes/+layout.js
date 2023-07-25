8598f4b13af2f20f27b10cac5cd2c9874497edce
Andre Esteves Perrone <contato@artneo.com.br>
2023-07-25 11:30:40 -0300

    .env

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,8 @@
+import { PUBLIC_URL_DATABASE } from "$env/static/public";
+
 export const load = async ({fetch}) => {
   const pegarProdutos = async () => {
-    const res = await fetch('http://mercadolocal.test/json/api/produtos');
+    const res = await fetch(`${PUBLIC_URL_DATABASE}/json/api/produtos`);
     const produtos = await res.json();
     return produtos;
   }
