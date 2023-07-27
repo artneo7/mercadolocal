44f863f6e7860ba4c45f134c5ae9d55e792f0f31
Andre Esteves Perrone <contato@artneo.com.br>
2023-07-27 15:36:12 -0300

    onMount()

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,23 @@
   import Header from "$lib/Header.svelte";
   import Footer from "$lib/Footer.svelte";
 	import Modal from '$lib/Modal.svelte';
+  import { dados } from "$lib/stores";
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+    // verificar dados no localStorage
+    if (localStorage.getItem('dados')) {
+      let data = JSON.parse(localStorage.getItem('dados'));
+      $dados.nome = data.nome;
+      $dados.email = data.email;
+      $dados.cep = data.cep;
+      $dados.numero = data.numero;
+      $dados.endereco = data.endereco;
+      $dados.bairro = data.bairro;
+      $dados.cidade = data.cidade;
+      $dados.estado = data.estado;
+    }
+  });
 </script>
 
 <div class="app">
